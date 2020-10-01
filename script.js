5439e7db15aebdb52d87c6f3f827ad8e3997b16c
William-vale <william_vale@alu.ufc.br>
2020-10-01 15:59:07 -0300

    Criando comida para cobrinha

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right"; //variavel que recebe a direção
+let food = { 
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,6 +23,11 @@ function criarCobrinha() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box); 
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -40,6 +49,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     //Variaveis que recebem a coordenada da direção da cobrinha
     let snakeX = snake[0].x;
