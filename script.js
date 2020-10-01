385ec7a3ce58d0225df540daf8a071679a5a2224
William-vale <william_vale@alu.ufc.br>
2020-10-01 14:11:49 -0300

    Definindo movimento da cobrinha

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
+let canvas = document.getElementById("snake"); //Recebe do HTML a tag canvas e recebe o id da mesma
+let context = canvas.getContext("2d"); //Interpreta o jogo como 2d
 let box = 32;
-let snake = [];
+let snake = []; //Array que recebe as direções da cobrinha no eixo x e y
 snake[0] = {
     x: 8*box,
     y: 8*box
 }
+
+let direction = "right"; //variavel que recebe a direção
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,5 +21,33 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    //Variaveis que recebem a coordenada da direção da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    //Direção para onde a cobrinha vai
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeX += box;
+
+    snake.pop(); // Retira o ultimo elemento do nosso array
+   
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //Acrescenta um quadradinho no primeiro elemento
+
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
+
